8350733e0ecb4af11a8d22606bc77348f7bd5a43
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-10-29 01:31:48 +0000

    guardado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
+/* eslint-disable
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+*/
+/*window.onload = function() {
+const myForm = document.getElementById("myForm")
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault():
+}*/
